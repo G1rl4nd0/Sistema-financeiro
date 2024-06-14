3aaac738ee7700486560ccf09765f3d47c7a6415
G1rl4nd0 <girlandojunior209@gmail.com>
2024-06-14 10:02:36 -0300

    ajustando devstreme

--- src/app/pages/financas/listagem/listagem.component.ts
+++ src/app/pages/financas/listagem/listagem.component.ts
@@ -1,80 +1,5 @@
 import { Component, Input, OnInit, Renderer2 } from '@angular/core';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-  tipo: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    position: 1,
-    name: 'Alimentação',
-    weight: 2024,
-    tipo: 'entrada',
-    symbol: '500,00',
-  },
-  {
-    position: 2,
-    name: 'Helium',
-    weight: 4.0026,
-    tipo: 'entrada',
-    symbol: 'He',
-  },
-  {
-    position: 3,
-    name: 'Lithium',
-    weight: 6.941,
-    tipo: 'entrada',
-    symbol: 'Li',
-  },
-  {
-    position: 4,
-    name: 'Beryllium',
-    weight: 9.0122,
-    tipo: 'entrada',
-    symbol: 'Be',
-  },
-  { position: 5, name: 'Boron', weight: 10.811, tipo: 'entrada', symbol: 'B' },
-  {
-    position: 6,
-    name: 'Carbon',
-    weight: 12.0107,
-    tipo: 'entrada',
-    symbol: 'C',
-  },
-  {
-    position: 7,
-    name: 'Nitrogen',
-    weight: 14.0067,
-    tipo: 'entrada',
-    symbol: 'N',
-  },
-  {
-    position: 8,
-    name: 'Oxygen',
-    weight: 15.9994,
-    tipo: 'entrada',
-    symbol: 'O',
-  },
-  {
-    position: 9,
-    name: 'Fluorine',
-    weight: 18.9984,
-    tipo: 'entrada',
-    symbol: 'F',
-  },
-  {
-    position: 10,
-    name: 'Neon',
-    weight: 20.1797,
-    tipo: 'entrada',
-    symbol: 'Ne',
-  },
-];
-
 @Component({
   selector: 'app-listagem',
   templateUrl: './listagem.component.html',
@@ -82,10 +7,91 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class ListagemComponent implements OnInit {
 
-  isDarkMode = false
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'tipo', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  dataSource: any[] = [
+    {
+      Id: 1,
+      Nome: 'Item 1',
+      Icone: 'icone1.png',
+      Ativo: true,
+      CriadoPor: {
+        Data: new Date(2024, 5, 12),
+        Nome: 'João',
+        Id: 101
+      },
+      ModificadoPor: {
+        Data: new Date(2024, 5, 13),
+        Nome: 'Maria',
+        Id: 102
+      }
+    },
+    {
+      Id: 2,
+      Nome: 'Item 1',
+      Icone: 'icone1.png',
+      Ativo: true,
+      CriadoPor: {
+        Data: new Date(2024, 5, 12),
+        Nome: 'João',
+        Id: 101
+      },
+      ModificadoPor: {
+        Data: new Date(2024, 5, 13),
+        Nome: 'Maria',
+        Id: 102
+      }
+    },
+    {
+      Id: 3,
+      Nome: 'Item 1',
+      Icone: 'icone1.png',
+      Ativo: true,
+      CriadoPor: {
+        Data: new Date(2024, 5, 12),
+        Nome: 'João',
+        Id: 101
+      },
+      ModificadoPor: {
+        Data: new Date(2024, 5, 13),
+        Nome: 'Maria',
+        Id: 102
+      }
+    },
+    {
+      Id: 4,
+      Nome: 'Item 1',
+      Icone: 'icone1.png',
+      Ativo: true,
+      CriadoPor: {
+        Data: new Date(2024, 5, 12),
+        Nome: 'João',
+        Id: 101
+      },
+      ModificadoPor: {
+        Data: new Date(2024, 5, 13),
+        Nome: 'Maria',
+        Id: 102
+      }
+    },
+    {
+      Id: 5,
+      Nome: 'Item 1',
+      Icone: 'icone1.png',
+      Ativo: true,
+      CriadoPor: {
+        Data: new Date(2024, 5, 12),
+        Nome: 'João',
+        Id: 101
+      },
+      ModificadoPor: {
+        Data: new Date(2024, 5, 13),
+        Nome: 'Maria',
+        Id: 102
+      }
+    },
+  ]
+
+  isDarkMode = false
 
   constructor(private renderer: Renderer2) {
     const theme = localStorage.getItem('theme');
@@ -110,8 +116,6 @@ export class ListagemComponent implements OnInit {
       this.renderer.removeClass(document.body, 'dark-mode');
     }
   }
-
-
 
   ngOnInit(): void {}
 }
